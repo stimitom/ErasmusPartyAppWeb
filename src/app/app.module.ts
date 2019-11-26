a00665795aee1289e127966f108a9929e5dbbf9d
tstim <e1551905@student.tuwien.ac.at>
2019-11-26 11:45:32 +0200

    Before starting auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AttendPartyComponent } from './attend-party/attend-party.component';
 import { VenueComponent } from './venues-list/venue/venue.component';
 import { VenueService } from './shared/venue.service';
 import { NationalityCardComponent } from './attend-party/nationality-card/nationality-card.component';
+import { HeaderComponent } from './header/header.component';
 
 
 
@@ -23,7 +24,8 @@ import { NationalityCardComponent } from './attend-party/nationality-card/nation
     VenuesListComponent,
     AttendPartyComponent,
     VenueComponent,
-    NationalityCardComponent
+    NationalityCardComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
