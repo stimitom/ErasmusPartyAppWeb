6b6e8acd329f64ef5ac42a1fe539ce1c149a26f6
tstim <e1551905@student.tuwien.ac.at>
2019-11-26 13:33:38 +0200

    s

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -1,3 +1,29 @@
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
+
+import { Observable,of } from 'rxjs';
+import { switchMap } from 'rxjs/operators'; 
+import { auth } from 'firebase';
+
 export class AuthService{ 
-    
+    user$: Observable<any>; 
+
+    constructor(private afAuth: AngularFireAuth, private db: AngularFirestore, private router: Router){
+        this.user$ = this.afAuth.authState.pipe(
+            switchMap( (user) => { 
+                if(user){ 
+                    return this.db.doc(`users/${user.uid}`).valueChanges(); 
+                }else{ 
+                    return of(null); 
+                }
+            })
+        ); 
+    }
+
+    signInEmail(){
+        this.afAuth.auth.signInWithEmailAndPassword('t.testaccount@hotmail.com','password').catch(function(error) {
+            // Handle Errors here.
+        })
+    }
 }
--- src/app/shared/user.model.ts
+++ src/app/shared/user.model.ts
@@ -1,3 +1,30 @@
 export class User{ 
     
+    userid: string;
+    username: string;
+    nationality: string;
+    email: string;
+    city: string;
+
+    listnames: Array<string>; 
+    counterpos0: number;
+    counterpos1: number;
+    counterpos2: number;
+    countermapping: Map<string,string>; 
+
+    constructor(userID:string, userName:string, nationality:string, email:string, city:string) {
+        this.userid = userID;
+        this.username = userName;
+        this.nationality = nationality;
+        this.email = email;
+        this.city = city;
+        
+        this.listnames = new Array<string>();
+        this.counterpos0 = 0; 
+        this.counterpos1 = 0; 
+        this.counterpos2 = 0; 
+        this.countermapping = new Map<string, string>();
+    }
+
+
 }
