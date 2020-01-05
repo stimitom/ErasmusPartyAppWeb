313bc048bbf1ba17e0b2d847f39022a4cdc57666
tstim <e1551905@student.tuwien.ac.at>
2020-01-05 12:23:16 +0100

    tabs working, material design

--- src/app/home/calender/calender.component.ts
+++ src/app/home/calender/calender.component.ts
@@ -1,21 +1,18 @@
-import { Component, OnInit, Input} from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation} from '@angular/core';
 import * as dateFormat from 'dateformat';
 import { CalenderService } from 'src/app/shared/calender.service';
+import { MatTabChangeEvent } from '@angular/material';
 
 @Component({
   selector: 'app-calender',
   templateUrl: './calender.component.html',
-  styleUrls: ['./calender.component.css']
+  styleUrls: ['./calender.component.css'], 
+  encapsulation: ViewEncapsulation.None
 })
 export class CalenderComponent implements OnInit {
   @Input() dateToday: string;
   @Input() dateTomorrow: string;
   @Input() dateTheDayAfterTomorrow: string;
-
-  activeToday:boolean; 
-  activeTomorrow:boolean; 
-  activeTheDayAfterTomorrow:boolean; 
-
 
   constructor(private calenderService:CalenderService) {
     this.getFormattedDates(); 
@@ -47,5 +44,13 @@ export class CalenderComponent implements OnInit {
     this.calenderService.dateSelected.emit(this.dateTheDayAfterTomorrow);
   }
 
-
+  onTabClicked(event: MatTabChangeEvent){ 
+    if (event.index == 0) {
+      this.activateToday(); 
+    }else if(event.index == 1){ 
+      this.activateTomorrow()
+    }else if (event.index == 2) {
+      this.activateTheDayAfterTomorrow(); 
+    } 
+  }
 }
