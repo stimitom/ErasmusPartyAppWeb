c9d201c34e06f840d2cf893bc97c5d298d385f8b
Stimitom <thomas@kodas.io>
2021-05-23 12:02:34 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
    firebaseConfig: {
-    apiKey: "AIzaSyBQjSGQc0isnJA21ZQ_kanZTnJl86x2mHg",
+    apiKey: "",
     authDomain: "erasmuspartyapp.firebaseapp.com",
     databaseURL: "https://erasmuspartyapp.firebaseio.com",
     projectId: "erasmuspartyapp",
     storageBucket: "erasmuspartyapp.appspot.com",
     messagingSenderId: "759157794385",
-    appId: "1:759157794385:web:a14ea3acd202e5542bba70",
+    appId: "",
     measurementId: "G-Y8Z35BY40N"
   }
 };
